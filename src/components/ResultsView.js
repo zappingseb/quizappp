43757a1da8590a8a9e7da9dfb8a80ee8465302a2
Sebastian Engel-Wolf <sebastian.engel-wolf@functionhr.com>
2025-09-16 12:44:53 +0200

    chore: fix image placement in aibnb

--- src/components/ResultsView.js
+++ src/components/ResultsView.js
@@ -101,7 +101,7 @@ const ResultsView = ({ phrases, answers, score, totalQuestions, onRestart, title
       />
       <Box>
         <Paper elevation={3} sx={{ mb: 3, p:2, display: 'flex', flexDirection: 'column' }}>
-          <Link target="new" href="https://www.airbnb.de/rooms/29823897?c=.pi80.pkYm9va2luZy9ndWVzdC9SZXNlcnZhdGlvbkNvbmZpcm1hdGlvblRlbXBsYXRl&euid=9f55bdee-e789-3cdd-23e1-de840d849edf&source_impression_id=p3_1758018458_P3otkhVibS2EUIxr">
+          <Link sx={{display: 'flex', flexDirection: 'column'}} target="new" href="https://www.airbnb.de/rooms/29823897?c=.pi80.pkYm9va2luZy9ndWVzdC9SZXNlcnZhdGlvbkNvbmZpcm1hdGlvblRlbXBsYXRl&euid=9f55bdee-e789-3cdd-23e1-de840d849edf&source_impression_id=p3_1758018458_P3otkhVibS2EUIxr">
           
             <img
               src={'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/330px-Airbnb_Logo_B%C3%A9lo.svg.png'}
